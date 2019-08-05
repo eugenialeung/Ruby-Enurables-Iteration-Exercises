175292182a0eb703d7c33fff135b13fde183a20a
eugenialeung <eleung316@gmail.com>
2019-08-05 14:17:55 -0700

    initial commit

---
+++ enumerables.rb
@@ -0,0 +1,90 @@
+class Array
+
+    def my_each(&prc)
+        self.length.times do |i|
+            prc.call(self[i])
+        end
+        self
+    end
+
+    def my_select(&prc)
+        selects = []
+        self.my_each do |item|
+            selects << item if prc.call(item)
+        end
+        selects
+    end
+
+    def my_reject(&prc)
+        selects = []
+        self.my_each do |item|
+            selects << item unless prc.call(item)
+        end
+        selects
+    end
+
+    def my_any?(&prc)
+        self.my_each do |item|
+            return true if prc.call(item)
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each do |item|
+            return false unless prc.call(item)
+        end
+        true
+    end
+
+    def my_flatten
+        flattened = []
+        self.my_each do |ele|
+            if ele.is_a?(Array)
+                flattened.concat(ele.my_flatten)
+            else
+                flattened << ele
+            end
+        end
+        flattened
+    end
+
+    def my_zip(*arrays) 
+        zipped = []
+        self.length.times do |i|
+            subzip = self[i]
+
+            arrays.my_each do |array|
+                subzip << array[i]
+            end
+            zipped << subzip
+        end
+        zipped
+    end
+
+    def my_rotate(positions = 1)
+        split_idx = positions % self.length
+
+        self.drop(split_idx) + self.take(split_idx)
+    end
+
+    def my_join(separator = "")
+        join = ""
+        self.length.times do |i|
+            join += self[i]
+            join += separator unless i == self.length - 1
+        end
+        join
+    end
+
+    def my_reverse
+        reversed = []
+        self.my_each do |ele|
+            reversed.unshift(ele)
+        end
+        reversed
+    end
+
+
+
+end
